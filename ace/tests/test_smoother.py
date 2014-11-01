d16330b8af86ea0c02839cdaaea9882c1b5019a1
Nick Touran <nick@partofthething.com>
2014-10-31 22:30:40 -0700

    Fixed smoother unit tests

--- ace/tests/test_smoother.py
+++ ace/tests/test_smoother.py
@@ -1,29 +1,23 @@
 '''
-Smoother tests
-
-Note: these aren't real unittests yet. Just making sure we can reproduce 
-the plots from the paper. 
+Smoother unit tests
 
 '''
-import math
 import unittest
-import random
-
-import numpy
-import pylab
 
 from ace import smoother
-from ace import supersmoother
 
 class TestSmoother(unittest.TestCase):
     def setUp(self):
         self.smoother = smoother.BasicFixedSpanSmoother()
-        self.smoother.specify_data_set(range(5), range(5))
+
         self.xData = [1.0, 2.0, 3.0]
         self.yData = [4.0, 5.0, 6.0]
-        self.smoother._update_mean_in_window(self.xData, self.yData)
-        self.smoother._update_variance_in_window(self.xData, self.yData)
+        self.smoother.specify_data_set(self.xData, self.yData)
         self.smoother.window_size = 3
+        self.smoother._update_values_in_window()
+        self.smoother._update_mean_in_window()
+        self.smoother._update_variance_in_window()
+
 
     def test_mean(self):
 
@@ -65,14 +59,14 @@ class TestSmoother(unittest.TestCase):
         """
         Make sure variance and covariance work when we remove an observation quickly
         """
-        self.smoother._remove_observation_to_variances(3, 6)
-        self.smoother._remove_observation_from_means(3, 6)
+        self.smoother._remove_observation(3, 6)
 
         cov_from_update = self.smoother._covariance_in_window
         var_from_update = self.smoother._variance_in_window
 
-        self.smoother._update_mean_in_window(self.xData[:2], self.yData[:2])
-        self.smoother._update_variance_in_window(self.xData[:2], self.yData[:2])
+        self.smoother._update_values_in_window()
+        self.smoother._update_mean_in_window()
+        self.smoother._update_variance_in_window()
 
         self.assertAlmostEqual(cov_from_update, self.smoother._covariance_in_window)
         self.assertAlmostEqual(var_from_update, self.smoother._variance_in_window)
@@ -81,18 +75,18 @@ class TestSmoother(unittest.TestCase):
         """
         Make sure variance and covariance work when we remove an observation quickly
         """
-        self.smoother._add_observation_to_means(7, 8)
-        self.smoother._add_observation_to_variances(7, 8)
-
+        self.smoother._add_observation(7, 8)
+        self.smoother.x.append(7)
+        self.smoother.y.append(8)
         cov_from_update = self.smoother._covariance_in_window
         var_from_update = self.smoother._variance_in_window
 
-        self.smoother._update_mean_in_window(self.xData + [7], self.yData + [8])
-        self.smoother._update_variance_in_window(self.xData + [7], self.yData + [8])
+        self.smoother._update_values_in_window()
+        self.smoother._update_mean_in_window()
+        self.smoother._update_variance_in_window()
 
         self.assertAlmostEqual(cov_from_update, self.smoother._covariance_in_window)
         self.assertAlmostEqual(var_from_update, self.smoother._variance_in_window)
-
 
 
 if __name__ == "__main__":
